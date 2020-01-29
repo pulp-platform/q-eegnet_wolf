30a45f82f6e783bfc8cb264d19cf5879de517743
Tibor Schneider <tiborschneider@bluewin.ch>
2020-01-29 11:25:49 +0100

    Added layer 1 test

---
+++ test/cl/net/layer1/cluster.c
@@ -0,0 +1,66 @@
+#include "stdio.h"
+#include "rt/rt_api.h"
+#include "test_stimuli.h"
+#include "../../../../src/cl/func/functional.h"
+#include "../../../../src/cl/net/net.h"
+#include "../../../../src/cl/net/layers.h"
+
+int do_bench(rt_perf_t* perf, int events) {
+
+    // allocate result memory
+    int8_t * p_output = rt_alloc(RT_ALLOC_L2_CL_DATA, sizeof(int8_t) * NET_F1 * NET_C * NET_T_ALIGN);
+    
+    //setup performance measurement
+    rt_perf_conf(perf, events);
+    
+    // start performance measurement
+    rt_perf_reset(perf);
+    rt_perf_start(perf);
+    
+    net_layer1(x_vec, p_output);
+
+    rt_perf_stop(perf);
+
+    int success = 0;
+    int8_t* p_output_tmp;
+    const int8_t* p_exp_tmp;
+    for (int k = 0; k < NET_F1; k++) {
+        for (int ch = 0; ch < NET_C; ch++) {
+            p_output_tmp = p_output + (k * NET_C + ch) * NET_T_ALIGN;
+            p_exp_tmp = y_exp_vec + (k * NET_C + ch) * NET_T_ALIGN;
+            for (int t = 0; t < NET_T; t++) {
+                if (*(p_output_tmp) != *(p_exp_tmp)) {
+                    printf("Fail at: [%d, %d, %d]: acq=%d exp=%d\n", k, ch, t, *p_output_tmp, *p_exp_tmp);
+                    success = 1;
+                }
+                p_output_tmp++;
+                p_exp_tmp++;
+            }
+        }
+    }
+
+    // free memory
+    rt_free(RT_ALLOC_L2_CL_DATA, (void*) p_output, sizeof(int8_t) * NET_F1 * NET_C * NET_T_ALIGN);
+
+    return success;
+}
+
+void cluster_entry(void* arg) {
+
+    // setup performance measurement
+    rt_perf_t perf;
+    rt_perf_init(&perf);
+
+    int result;
+
+    result = do_bench(&perf, (1<<RT_PERF_CYCLES | 1<<RT_PERF_INSTR));
+
+    // print the results
+    if (result == 0) {
+        printf("## 1: result: OK\n");
+    } else {
+        printf("## 1: result: FAIL\n");
+    }
+    printf("## 1: cycles: %d\n", rt_perf_read(RT_PERF_CYCLES));
+    printf("## 1: instructions: %d\n", rt_perf_read(RT_PERF_INSTR));
+}
---
+++ test/cl/net/layer1/testcase.py
@@ -0,0 +1,74 @@
+"""
+This file will test the convolution implementation
+"""
+
+import random
+import os
+import numpy as np
+from test_utils import parse_output, TestLogger
+from header_file import HeaderFile, HeaderConstant, HeaderArray, align_array
+from makefile import Makefile
+from golden_model import GoldenModel
+
+TESTNAME = "cl::net::layer1"
+RESULT_FILE = "result.out"
+
+NET_FILENAME = "../../../../data/net.npz"
+CONFIG_FILENAME = "../../../../data/config.json"
+
+
+def gen_stimuli(rnd=False):
+    """
+    This function generates the stimuli (input and output) for the test
+    """
+    model = GoldenModel(CONFIG_FILENAME, NET_FILENAME, round=rnd, clip_balanced=False)
+    x = np.random.randint(-127, 127, (model.C, model.T))
+    y_exp = model.layers[0](x)
+    return x, y_exp
+
+
+def test():
+    """
+    Execute the tests
+    Returns: (n_total, n_success)
+    """
+
+    logger = TestLogger(TESTNAME)
+
+    for rnd in [False, True]:
+
+        # generate makefile
+        mkf = Makefile()
+        mkf.add_fc_test_source("test.c")
+        mkf.add_cl_test_source("cluster.c")
+        mkf.add_cl_prog_source("net/layer1.c")
+        mkf.add_cl_prog_source("net/net.c")
+        mkf.add_cl_prog_source("func/conv.c")
+        mkf.add_cl_prog_source("func/transform.c")
+        if rnd:
+            mkf.add_define("ROUND")
+        mkf.write()
+
+        # generate the stimuli
+        x, y_exp = gen_stimuli(rnd)
+        x_align = align_array(x)
+        y_exp_align = align_array(y_exp)
+
+        # prepare header file
+        header = HeaderFile("test_stimuli.h")
+        header.add(HeaderArray("x_vec", "int8_t", x_align.ravel()))
+        header.add(HeaderArray("y_exp_vec", "int8_t", y_exp_align.ravel()))
+        header.write()
+
+        # compile and run
+        os.system("make clean all run > {}".format(RESULT_FILE))
+
+        # parse output
+        result = parse_output(RESULT_FILE)
+
+        # log the result
+        subcase_name = "round" if rnd else "floor"
+        logger.show_subcase_result(subcase_name, result)
+
+    # return summary
+    return logger.summary()
