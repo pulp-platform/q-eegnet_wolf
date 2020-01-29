b13adb7b03b9b48bd5fa7a49257de557994c4bc0
Tibor Schneider <tiborschneider@bluewin.ch>
2020-01-29 13:33:53 +0100

    Refactor: local variables start with _

--- src/cl/func/transform.c
+++ src/cl/func/transform.c
@@ -31,131 +31,130 @@ void func_transform_32to8(const int32_t* p_in,
                           unsigned int stride,
                           int8_t* p_res) {
 
-    int32_t a, b, c, d;             // temporary values
-    const int32_t* p_x = p_in;      // pointer to current element in x
-    int8_t* p_y = p_res;            // pointer to the output element
+    int32_t _a, _b, _c, _d;             // temporary values
+    const int32_t* _p_x = p_in;      // pointer to current element in x
 
 #ifdef ROUND
-    uint32_t sa, sb, sc, sd;                          // sign bit of a, b, c and d
-    int32_t offset = div_factor / 2;                  // offset is added every time
-    int32_t neg_offset = 1 - ((div_factor / 2) * 2);  // neg offset is only added when the number if negative
+    uint32_t _sa, _sb, _sc, _sd;                          // sign bit of a, b, c and d
+    int32_t _offset = div_factor / 2;                  // offset is added every time
+    int32_t _neg_offset = 1 - ((div_factor / 2) * 2);  // neg offset is only added when the number if negative
 #endif //ROUND
 
-    unsigned int num_blk = len / 4;
-    unsigned int num_rem = len % 4;
+    unsigned int _num_blk = len / 4;
+    unsigned int _num_rem = len % 4;
 
     // do the elements which can be unrolled
-    while (num_blk > 0) {
+    while (_num_blk > 0) {
 
-        a = *p_x;
-        b = *(p_x + 1 * stride);
-        c = *(p_x + 2 * stride);
-        d = *(p_x + 3 * stride);
+        _a = *_p_x;
+        _b = *(_p_x + 1 * stride);
+        _c = *(_p_x + 2 * stride);
+        _d = *(_p_x + 3 * stride);
 
-        p_x += 4 * stride;
+        _p_x += 4 * stride;
 
 #ifdef ROUND
-        sa = SIGN_BIT(a);
-        a += offset;
-        a = __MAC(a, sa, neg_offset);
+        _sa = SIGN_BIT(_a);
+        _a += _offset;
+        _a = __MAC(_a, _sa, _neg_offset);
 
-        sb = SIGN_BIT(b);
-        b += offset;
-        b = __MAC(b, sb, neg_offset);
+        _sb = SIGN_BIT(_b);
+        _b += _offset;
+        _b = __MAC(_b, _sb, _neg_offset);
 
-        sc = SIGN_BIT(c);
-        c += offset;
-        c = __MAC(c, sc, neg_offset);
+        _sc = SIGN_BIT(_c);
+        _c += _offset;
+        _c = __MAC(_c, _sc, _neg_offset);
 
-        sd = SIGN_BIT(d);
-        d += offset;
-        d = __MAC(d, sd, neg_offset);
+        _sd = SIGN_BIT(_d);
+        _d += _offset;
+        _d = __MAC(_d, _sd, _neg_offset);
 #endif //ROUND
 
-        a = a / div_factor;
-        b = b / div_factor;
-        c = c / div_factor;
-        d = d / div_factor;
+        _a = _a / div_factor;
+        _b = _b / div_factor;
+        _c = _c / div_factor;
+        _d = _d / div_factor;
 
-        a = __CLIP_R(a, 127);
-        b = __CLIP_R(b, 127);
-        c = __CLIP_R(c, 127);
-        d = __CLIP_R(d, 127);
+        _a = __CLIP_R(_a, 127);
+        _b = __CLIP_R(_b, 127);
+        _c = __CLIP_R(_c, 127);
+        _d = __CLIP_R(_d, 127);
 
-        *((int32_t*)p_res) = (int32_t)__PACK4(a, b, c, d);
+        *((int32_t*)p_res) = (int32_t)__PACK4(_a, _b, _c, _d);
         
         p_res += 4;
-        num_blk--;
+        _num_blk--;
     }
 
-    if (num_rem == 1) {
+    if (_num_rem == 1) {
 
-        a = *p_x;
-
-#ifdef ROUND
-        sa = SIGN_BIT(a);
-        a += offset;
-        a = __MAC(a, sa, neg_offset);
-#endif //ROUND
-
-        a = a / div_factor;
-
-        a = __CLIP_R(a, 127);
-
-        *((int32_t*)p_res) = (int32_t)__PACK4(a, 0, 0, 0);
-
-    } else if (num_rem == 2) {
-
-        a = *p_x;
-        b = *(p_x + 1 * stride);
+        _a = *_p_x;
 
 #ifdef ROUND
-        sa = SIGN_BIT(a);
-        a += offset;
-        a = __MAC(a, sa, neg_offset);
-
-        sb = SIGN_BIT(b);
-        b += offset;
-        b = __MAC(b, sb, neg_offset);
+        _sa = SIGN_BIT(_a);
+        _a += _offset;
+        _a = __MAC(_a, _sa, _neg_offset);
 #endif //ROUND
 
-        a = a / div_factor;
-        b = b / div_factor;
+        _a = _a / div_factor;
 
-        a = __CLIP_R(a, 127);
-        b = __CLIP_R(b, 127);
+        _a = __CLIP_R(_a, 127);
 
-        *((int32_t*)p_res) = (int32_t)__PACK4(a, b, 0, 0);
+        *((int32_t*)p_res) = (int32_t)__PACK4(_a, 0, 0, 0);
 
-    } else if (num_rem == 3) {
+    } else if (_num_rem == 2) {
 
-        a = *p_x;
-        b = *(p_x + 1 * stride);
-        c = *(p_x + 2 * stride);
+        _a = *_p_x;
+        _b = *(_p_x + 1 * stride);
 
 #ifdef ROUND
-        sa = SIGN_BIT(a);
-        a += offset;
-        a = __MAC(a, sa, neg_offset);
+        _sa = SIGN_BIT(_a);
+        _a += _offset;
+        _a = __MAC(_a, _sa, _neg_offset);
 
-        sb = SIGN_BIT(b);
-        b += offset;
-        b = __MAC(b, sb, neg_offset);
-
-        sc = SIGN_BIT(c);
-        c += offset;
-        c = __MAC(c, sc, neg_offset);
+        _sb = SIGN_BIT(_b);
+        _b += _offset;
+        _b = __MAC(_b, _sb, _neg_offset);
 #endif //ROUND
 
-        a = a / div_factor;
-        b = b / div_factor;
-        c = c / div_factor;
+        _a = _a / div_factor;
+        _b = _b / div_factor;
 
-        a = __CLIP_R(a, 127);
-        b = __CLIP_R(b, 127);
-        c = __CLIP_R(c, 127);
+        _a = __CLIP_R(_a, 127);
+        _b = __CLIP_R(_b, 127);
 
-        *((int32_t*)p_res) = (int32_t)__PACK4(a, b, c, 0);
+        *((int32_t*)p_res) = (int32_t)__PACK4(_a, _b, 0, 0);
+
+    } else if (_num_rem == 3) {
+
+        _a = *_p_x;
+        _b = *(_p_x + 1 * stride);
+        _c = *(_p_x + 2 * stride);
+
+#ifdef ROUND
+        _sa = SIGN_BIT(_a);
+        _a += _offset;
+        _a = __MAC(_a, _sa, _neg_offset);
+
+        _sb = SIGN_BIT(_b);
+        _b += _offset;
+        _b = __MAC(_b, _sb, _neg_offset);
+
+        _sc = SIGN_BIT(_c);
+        _c += _offset;
+        _c = __MAC(_c, _sc, _neg_offset);
+#endif //ROUND
+
+        _a = _a / div_factor;
+        _b = _b / div_factor;
+        _c = _c / div_factor;
+
+        _a = __CLIP_R(_a, 127);
+        _b = __CLIP_R(_b, 127);
+        _c = __CLIP_R(_c, 127);
+
+        *((int32_t*)p_res) = (int32_t)__PACK4(_a, _b, _c, 0);
     }
 }
 
@@ -182,142 +181,141 @@ void func_transform_32to8_bias(const int32_t* p_in,
                                  unsigned int stride,
                                  int8_t* p_res) {
 
-    int32_t a, b, c, d;             // temporary values
-    const int32_t* p_x = p_in;      // pointer to current element in x
-    int8_t* p_y = p_res;            // pointer to the output element
+    int32_t _a, _b, _c, _d;          // temporary values
+    const int32_t* _p_x = p_in;      // pointer to current element in x
 
 #ifdef ROUND
-    uint32_t sa, sb, sc, sd;                          // sign bit of a, b, c and d
-    int32_t offset = div_factor / 2;                  // offset is added every time
-    int32_t neg_offset = -((div_factor / 2) * 2) + 1; // neg offset is only added when the number if negative
+    uint32_t _sa, _sb, _sc, _sd;                      // sign bit of a, b, c and d
+    int32_t _offset = div_factor / 2;                 // offset is added every time
+    int32_t _neg_offset = 1 - ((div_factor / 2) * 2); // neg offset is only added when the number if negative
 #endif //ROUND
 
-    unsigned int num_blk = len / 4;
-    unsigned int num_rem = len % 4;
+    unsigned int _num_blk = len / 4;
+    unsigned int _num_rem = len % 4;
 
     // do the elements which can be unrolled
-    while (num_blk > 0) {
+    while (_num_blk > 0) {
 
-        a = *p_x;
-        b = *(p_x + 1 * stride);
-        c = *(p_x + 2 * stride);
-        d = *(p_x + 3 * stride);
+        _a = *_p_x;
+        _b = *(_p_x + 1 * stride);
+        _c = *(_p_x + 2 * stride);
+        _d = *(_p_x + 3 * stride);
 
-        a += bias;
-        b += bias;
-        c += bias;
-        d += bias;
+        _a += bias;
+        _b += bias;
+        _c += bias;
+        _d += bias;
 
 #ifdef ROUND
-        sa = SIGN_BIT(a);
-        a += offset;
-        a = __MAC(a, sa, neg_offset);
+        _sa = SIGN_BIT(_a);
+        _a += _offset;
+        _a = __MAC(_a, _sa, _neg_offset);
 
-        sb = SIGN_BIT(b);
-        b += offset;
-        b = __MAC(b, sb, neg_offset);
+        _sb = SIGN_BIT(_b);
+        _b += _offset;
+        _b = __MAC(_b, _sb, _neg_offset);
 
-        sc = SIGN_BIT(c);
-        c += offset;
-        c = __MAC(c, sc, neg_offset);
+        _sc = SIGN_BIT(_c);
+        _c += _offset;
+        _c = __MAC(_c, _sc, _neg_offset);
 
-        sd = SIGN_BIT(d);
-        d += offset;
-        d = __MAC(d, sd, neg_offset);
+        _sd = SIGN_BIT(_d);
+        _d += _offset;
+        _d = __MAC(_d, _sd, _neg_offset);
 #endif //ROUND
 
-        a = a / div_factor;
-        b = b / div_factor;
-        c = c / div_factor;
-        d = d / div_factor;
+        _a = _a / div_factor;
+        _b = _b / div_factor;
+        _c = _c / div_factor;
+        _d = _d / div_factor;
 
-        a = __CLIP_R(a, 127);
-        b = __CLIP_R(b, 127);
-        c = __CLIP_R(c, 127);
-        d = __CLIP_R(d, 127);
+        _a = __CLIP_R(_a, 127);
+        _b = __CLIP_R(_b, 127);
+        _c = __CLIP_R(_c, 127);
+        _d = __CLIP_R(_d, 127);
 
-        *((int32_t*)p_res) = (int32_t)__PACK4(a, b, c, d);
+        *((int32_t*)p_res) = (int32_t)__PACK4(_a, _b, _c, _d);
         
         p_res += 4;
-        p_x += 4 * stride;
-        num_blk--;
+        _p_x += 4 * stride;
+        _num_blk--;
     }
 
-    if (num_rem == 1) {
+    if (_num_rem == 1) {
 
-        a = *p_x;
-        a += bias;
+        _a = *_p_x;
+        _a += bias;
 #ifdef ROUND
-        sa = SIGN_BIT(a);
-        a += offset;
-        a = __MAC(a, sa, neg_offset);
+        _sa = SIGN_BIT(_a);
+        _a += _offset;
+        _a = __MAC(_a, _sa, _neg_offset);
 #endif //ROUND
 
-        a = a / div_factor;
+        _a = _a / div_factor;
 
-        a = __CLIP_R(a, 127);
+        _a = __CLIP_R(_a, 127);
 
-        *((int32_t*)p_res) = (int32_t)__PACK4(a, 0, 0, 0);
+        *((int32_t*)p_res) = (int32_t)__PACK4(_a, 0, 0, 0);
 
-    } else if (num_rem == 2) {
+    } else if (_num_rem == 2) {
 
-        a = *p_x;
-        b = *(p_x + 1 * stride);
+        _a = *_p_x;
+        _b = *(_p_x + 1 * stride);
 
-        a += bias;
-        b += bias;
-
-#ifdef ROUND
-        sa = SIGN_BIT(a);
-        a += offset;
-        a = __MAC(a, sa, neg_offset);
-
-        sb = SIGN_BIT(b);
-        b += offset;
-        b = __MAC(b, sb, neg_offset);
-#endif //ROUND
-
-        a = a / div_factor;
-        b = b / div_factor;
-
-        a = __CLIP_R(a, 127);
-        b = __CLIP_R(b, 127);
-
-        *((int32_t*)p_res) = (int32_t)__PACK4(a, b, 0, 0);
-
-    } else if (num_rem == 3) {
-
-        a = *p_x;
-        b = *(p_x + 1 * stride);
-        c = *(p_x + 2 * stride);
-
-        a += bias;
-        b += bias;
-        c += bias;
+        _a += bias;
+        _b += bias;
 
 #ifdef ROUND
-        sa = SIGN_BIT(a);
-        a += offset;
-        a = __MAC(a, sa, neg_offset);
+        _sa = SIGN_BIT(_a);
+        _a += _offset;
+        _a = __MAC(_a, _sa, _neg_offset);
 
-        sb = SIGN_BIT(b);
-        b += offset;
-        b = __MAC(b, sb, neg_offset);
-
-        sc = SIGN_BIT(c);
-        c += offset;
-        c = __MAC(c, sc, neg_offset);
+        _sb = SIGN_BIT(_b);
+        _b += _offset;
+        _b = __MAC(_b, _sb, _neg_offset);
 #endif //ROUND
 
-        a = a / div_factor;
-        b = b / div_factor;
-        c = c / div_factor;
+        _a = _a / div_factor;
+        _b = _b / div_factor;
 
-        a = __CLIP_R(a, 127);
-        b = __CLIP_R(b, 127);
-        c = __CLIP_R(c, 127);
+        _a = __CLIP_R(_a, 127);
+        _b = __CLIP_R(_b, 127);
 
-        *((int32_t*)p_res) = (int32_t)__PACK4(a, b, c, 0);
+        *((int32_t*)p_res) = (int32_t)__PACK4(_a, _b, 0, 0);
+
+    } else if (_num_rem == 3) {
+
+        _a = *_p_x;
+        _b = *(_p_x + 1 * stride);
+        _c = *(_p_x + 2 * stride);
+
+        _a += bias;
+        _b += bias;
+        _c += bias;
+
+#ifdef ROUND
+        _sa = SIGN_BIT(_a);
+        _a += _offset;
+        _a = __MAC(_a, _sa, _neg_offset);
+
+        _sb = SIGN_BIT(_b);
+        _b += _offset;
+        _b = __MAC(_b, _sb, _neg_offset);
+
+        _sc = SIGN_BIT(_c);
+        _c += _offset;
+        _c = __MAC(_c, _sc, _neg_offset);
+#endif //ROUND
+
+        _a = _a / div_factor;
+        _b = _b / div_factor;
+        _c = _c / div_factor;
+
+        _a = __CLIP_R(_a, 127);
+        _b = __CLIP_R(_b, 127);
+        _c = __CLIP_R(_c, 127);
+
+        *((int32_t*)p_res) = (int32_t)__PACK4(_a, _b, _c, 0);
     }
 
 }
